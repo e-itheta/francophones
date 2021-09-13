9a1e0c433b81951d0e1b804f35407358cd535713
Kevin Lai <zlnh4@umsystem.edu>
2021-09-13 07:33:03 -0500

    link

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -1,4 +1,5 @@
 import * as React from "react"
+import "./app.css"
 
 export default
 function App(props: any)
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import App from "./components/app"
 import "./index.css"
 
 ReactDOM.render(
-    <App>Placeholder</App>,
+    <App>
+        <h3>
+            Site under construction. Don't @ me... 
+        </h3>
+        <a href="https://discord.gg/3K8dEk2Hs8">
+            unless it's through the club discord
+        </a>
+    </App>,
     document.getElementById("root")
 )
